e4ef9fcba0fc9a665f2e5bb8e2780959a98c4f59
Aaryan Dewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-17 08:35:34 +0000

    Update Counter.js

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,12 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 
-import { selectCount } from "./counterSlice";
-
+import { selectCount, increment } from "./counterSlice";
 export function Counter() {
   const count = useSelector(selectCount);
+  const dispatch = useDispatch();
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello {count}</h1>
+      <button onClick={() => dispatch(increment())}>Inc</button>
+      <button>Dec</button>
     </>
   );
 }
