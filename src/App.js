18f8e18d8fa506fd09b1fa7480932923ffc2f8fd
Aaryan Dewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-18 05:35:26 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./styles.css";
-import { Counter } from "./features/counter/Counter";
-import Aaryan from "./components/Aaryan";
+// import { Counter } from "./features/counter/Counter";
+// import Aaryan from "./components/Aaryan";
 export default function App() {
   return (
     <div className="App">
-      <Counter />
-      <Aaryan /> //works
+      {/* <Counter /> */}
+      {/* <Aaryan /> //works */}
+      hello
     </div>
   );
 }
--- src/components/Aaryan.js
+++
@@ -1,8 +0,0 @@
-import { useSelector } from "react-redux";
-
-import { selectCount } from "../features/counter/counterSlice";
-
-export default function Aaryan() {
-  const aaryanCounter = useSelector(selectCount);
-  return <h1>Count is eual to {aaryanCounter}</h1>;
-}
